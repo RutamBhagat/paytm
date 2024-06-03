a52b7c46d7151ba686ecf284512150df866e04d0
RutamBhagat <rutambhagat@gmail.com>
2024-06-03 17:31:08 +0530

    add httpx

--- src/app/api/v1/accounts.py
+++ src/app/api/v1/accounts.py
@@ -1,5 +1,4 @@
 import httpx
-import aiohttp
 import asyncio
 from fastapi import APIRouter, status
 from src.app.schemas.accounts_schema import AccountsResponse, TransferRequest
@@ -61,17 +60,4 @@ async def get_random_todos():
         tasks = [client.get(url) for url in urls]
         responses = await asyncio.gather(*tasks)
     todos = [response.json() for response in responses]
-    return todos
-
-
-@router.get("/get_random_todos_with_aiohttp", status_code=status.HTTP_200_OK)
-async def get_random_todos_with_aiohttp():
-    urls = ["https://sum-server.100xdevs.com/todos"] * 10
-    async with aiohttp.ClientSession() as session:
-        tasks = [session.get(url) for url in urls]
-        responses = await asyncio.gather(*tasks)
-    todos = []
-    for response in responses:
-        data = await response.json()
-        todos.append(data)
     return todos
